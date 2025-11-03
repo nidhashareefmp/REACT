2017a969e3b0e6bc19a20cb1fd2da312200ba61b
nidhashareefmp <nidhashareef2004@gmail.com>
2025-11-03 13:03:47 +0530

    nidha

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Welcome from './embedding-expression/Welcome'
+// import Basic from './Basic'
+
+function App() {
+
+  return (
+    <>
+      
+      {/*-------------- {embedding expression}------------- */}
+      {/* <Basic/> */}
+      <Welcome/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Basic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Basic = () => {
+  return (
+    <div>
+      <h2>Welcome to REACT</h2>
+    </div>
+  )
+}
+
+export default Basic
