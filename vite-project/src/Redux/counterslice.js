c3b5e44001c685819f457c4d30d83ad17ae80953
nidhashareefmp <nidhashareef2004@gmail.com>
2025-12-01 12:59:41 +0530

    first

---
+++ vite-project/src/Redux/counterslice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterslice = createSlice({
+    name :'counter',
+    initialState:{ value : 0},
+    reducers :{
+        increment: (state) => {state.value +=  1},
+        decrement: (state) => {state.value -= 1}
+
+    }
+})
+
+export const {increment,decrement} = counterslice.actions;
+export default counterslice.reducers;
